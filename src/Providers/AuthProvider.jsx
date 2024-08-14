00a0095a8a76e07db2a0b2f2f066bc5a2db643bc
sayemhossin <sayemhossin22@gmail.com>
2024-08-15 00:47:04 +0600

    added firebase section

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,71 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import auth from "../Firebase/Firebase";
+
+export const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState('')
+    const [loading,setLoading] = useState(true)
+    
+    const createUser = (email,password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const signIn = (email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+
+    const googleLogin = ()=>{
+        return signInWithPopup(auth, googleProvider)
+     }
+    
+  
+     const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe =  onAuthStateChanged(auth, user =>{
+            
+            setLoading(false)
+            setUser(user)
+    
+            
+          })
+          return () =>{
+              unSubscribe()
+          }
+      },[])
+    
+    const authInfo = {
+        createUser,
+        signIn,
+        user,
+        logOut,
+        loading,
+        googleLogin,
+    
+    }
+    
+    return (
+        <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
+    );
+};
+
+
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
+
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Root from './Root/Root';
 import Home from './Home/Home';
+import AuthProvider from './Providers/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
